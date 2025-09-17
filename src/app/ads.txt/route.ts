ed15aacfbcb57bb818cbe20c14a15a574c3f68dd
idoubi <me@idoubi.cc>
2025-09-17 17:43:22 +0800

    add ads.txt

---
+++ src/app/ads.txt/route.ts
@@ -0,0 +1,31 @@
+import { getAllConfigs } from "@/services/config";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const configs = await getAllConfigs();
+
+    if (!configs.adsense_code) {
+      throw new Error("adsense_code is not set");
+    }
+
+    const adsenseCode = configs.adsense_code.replace("ca-", "");
+
+    const adsContent = `google.com, ${adsenseCode}, DIRECT, f08c47fec0942fa0`;
+
+    return new NextResponse(adsContent, {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  } catch (error) {
+    console.error("get ads.txt failed:", error);
+    return new NextResponse("", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  }
+}
